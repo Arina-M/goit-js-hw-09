a66f84964f6e7aada2484feb8bef3a370164d894
Arina Miahkykh <79450238+Arina-M@users.noreply.github.com>
2023-05-01 11:57:04 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const TIME_COLOR_CHANGES = 1000;
+let changeColorTime = null;
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+function getRandomHexColor() {
+   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+startBtn.addEventListener('click', onClickStartBtn);
+stopBtn.addEventListener('click', onClickStopBtn);
+
+
+function onClickStartBtn() {
+    changeColorTime = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+        startBtn.setAttribute('disabled', 'disabled');
+        stopBtn.removeAttribute('disabled');
+    }, TIME_COLOR_CHANGES);
+}
+
+function onClickStopBtn() {
+    clearInterval(changeColorTime);
+    startBtn.removeAttribute('disabled');
+    stopBtn.setAttribute('disabled', 'disabled');
+}
