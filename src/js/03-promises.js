4be3f85a3f8529ec62c4ecc2ae0d4e610fd0cce1
Arina Miahkykh <79450238+Arina-M@users.noreply.github.com>
2023-05-02 11:03:51 +0300

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,65 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  formEl: document.querySelector('.form'),
+}
+
+refs.formEl.addEventListener('input', onFormInputValue);
+refs.formEl.addEventListener('submit', onPromisesStart);
+
+let delayTime = 0;
+let amount = 0;
+let stepTime = 0;
+
+
+function onFormInputValue(evt) {
+  if(evt.target.name === 'delay'){
+    delayTime = Number(evt.target.value);
+  }
+
+  if(evt.target.name === 'amount'){
+    amount = Number(evt.target.value);
+  }
+
+  if(evt.target.name === 'step'){
+    stepTime = Number(evt.target.value);
+  }
+};
+
+function onPromisesStart(evt) {
+  evt.preventDefault()
+  if(delayTime <=0 || amount <= 0 || stepTime <= 0){
+    Notiflix.Notify.failure('First delay, Delay step and Amount must be greater than 0!');
+  } else {
+    makePromises(delayTime, amount, stepTime)
+  }
+};
+
+
+function makePromises(delayValue, amountValue, stepValue) {
+  let delay = delayValue;
+  let i = 1;
+  while (i <= amountValue) {
+    createPromise(i, delay).then((value) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${value.position} in ${value.delay}ms`);
+    }).catch((value) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${value.position} in ${value.delay}ms`);
+    });
+    delay += stepValue;
+    i += 1;
+  }
+}
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => { 
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay })
+      } else {
+        reject({ position, delay })
+      }
+    }, delay);
+  });
 }
